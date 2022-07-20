d86fde27353d8b9a0894d2dfe40d5c1001b18add
“danielwarga” <danielwarga@gmail.com>
2022-07-20 15:04:33 +0100

    data.js upload

---
+++ server/data.js
@@ -0,0 +1,14 @@
+const websites = [
+    "https://www.amazon.co.uk/",
+    "https://github.com/",
+    "https://www.youtube.com/",
+    "https://www.facebook.com/",
+    "https://www.triwa.com/",
+    "https://unsplash.com/",
+    "https://www.nationalgeographic.com",
+    "https://www.w3schools.com/",
+    "https://getbootstrap.com/",
+    "https://css-tricks.com/"
+] 
+
+module.exports = websites;
