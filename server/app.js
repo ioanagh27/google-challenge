1d5b1c1b21be3b6c6bdcd8cc8e55a642b26d17df
“danielwarga” <danielwarga@gmail.com>
2022-07-20 14:47:15 +0100

    Add data file

--- server/app.js
+++ server/app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
+const data = require("./websites")
+app.use(express.json());
 
+
+// app.get("./", (req, res) =>{
+//     res.status(200).send("asdf")
+// })
 
 
 module.exports = app;
