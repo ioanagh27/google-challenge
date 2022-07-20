ee56b69ebdf648444a6dd2d7db5dd643e3c3949a
“danielwarga” <danielwarga@gmail.com>
2022-07-20 15:17:32 +0100

    working apis

--- server/app.js
+++ server/app.js
@@ -6,18 +6,22 @@ app.use(express.json());
 const data = require('./data');
 
 
-console.log(data)
+function getRandomWebsite(arr) {
+    let randomWebsite = Math.floor(Math.random() * arr.length);
+    return arr[randomWebsite];
+  }
 
 app.get("/", (req, res) =>{
     res.status(200).send('Welcome to our fake Google site!')
 })
 
 app.get('/websites', (req, res) => {
-    let websites = data;
-    res.json({
-        websites: websites.map(w =>w['website'])
-    })
+    res.status(200).send(data);
 })
 
+app.get('/websites/random', (req, res) => {
+    const randomWebsite = getRandomWebsite(data);
+    res.status(200).send(randomWebsite);
+})
 
 module.exports = app;
